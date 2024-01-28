05e45101a4e32f37278939a38af698cee12e21fb
naulbaragi <fgfg8292@gmail.com>
2024-01-29 04:03:14 +0900

    루틴 플래닝 기능 추가 및 기능 추가

--- src/Modal/ButtonModal.ts
+++ src/Modal/ButtonModal.ts
@@ -1,5 +1,10 @@
-import { Modal, App, Setting } from 'obsidian';
+import { Modal, App, Setting, Notice, moment, stringifyYaml } from 'obsidian';
 import WorkoutPlugin from 'main';
+import { Markdown } from 'src/Markdown/Markdown';
+import { RoutineModel } from 'src/Workout/Routine/RoutineModel';
+import { RoutineUpdate } from 'src/Workout/Routine/RoutineUpdate';
+
+//this is workout day using modal
 
 export class WorkoutButtonModal extends Modal {
     plugin: WorkoutPlugin;
@@ -18,11 +23,51 @@ export class WorkoutButtonModal extends Modal {
             .addButton((btn) =>
                 btn.setButtonText('OK').onClick(async () => {
                     //이부분에 데이터 파싱후 오늘의 계획 표 생성
+                    // this.close();
+                    // await new RoutineUpdate(this.plugin).todayRoutineMaker();
+                    new Notice('test!');
+
+                    // 스타팅 데이와 일치하는 부분이 있는지 확인
+                    // 아니라면 며칠 뒤 운동이 시작되는지 가져오는 기능
+
+                    //아직 루틴이 설정되지 않았습니다.
+                    const today = moment().format('YYYY-MM-DD');
+                    // 수정
+                    if (this.plugin.settings.startday !== today){
+                        new Notice('아직 루틴이 설정되지 않았습니다');
+                    }
+
+                    await new RoutineUpdate(this.plugin).todayRoutineUpdater();
+                    const workoutProperites: RoutineModel = {
+                        Today: moment().format(),
+                        Workout: true,
+                        Program: this.plugin.settings.routineTemplate.name,
+                        Session: this.plugin.settings.todayRoutine.sessionname,
+                        Workoutvolumn: 2000,
+                        Bodyweight: parseFloat(this.plugin.settings.bodyWeight),
+                        Bigthree: this.plugin.settings.bigThree[3],
+                        Squat1rm: this.plugin.settings.bigThree[0],
+                        Benchpress1rm: this.plugin.settings.bigThree[1],
+                        Deadlift1rm: this.plugin.settings.bigThree[2],
+                    };
+                    //오늘 할 워크아웃 목록 가져오기
+                    const contextData = await new RoutineUpdate(this.plugin).workoutContextMaker();
+                    console.log(contextData);
+
+                    const tempdata = `---\n${stringifyYaml(workoutProperites)}---\n` + contextData;
+                    try {
+                        new Markdown(this.plugin, this.app).createNote(`Workout ${today}`, tempdata);
+                        
+                    } catch (error) {
+                        new Notice(error);
+                    }
+
                     this.close();
                 }),
             )
             .addButton((btn) =>
                 btn.setButtonText('Cancel').onClick(async () => {
+                    console.log('tt');
                     this.close();
                 }),
             );
--- src/Modal/FirstButtonModal.ts
+++ src/Modal/FirstButtonModal.ts
@@ -1,6 +1,7 @@
 import { Modal, App, Setting, Notice } from 'obsidian';
 import WorkoutPlugin from 'main';
 import { Markdown } from 'src/Markdown/Markdown';
+import { RoutineUpdate } from 'src/Workout/Routine/RoutineUpdate';
 
 export class FirstWorkoutButtonModal extends Modal {
     plugin: WorkoutPlugin;
@@ -24,10 +25,15 @@ export class FirstWorkoutButtonModal extends Modal {
 
         new Setting(contentEl).addButton((btn) =>
             btn.setButtonText('Submit').onClick(async () => {
-                this.close();
+                // this.close();
                 this.plugin.settings.startday = this.startday;
+                //삭제 예정?
+                //프로그래스를 퍼센트로 나타내주면 좋을듯
+
+                this.plugin.settings.todayRoutine.progress = [1,1];
                 console.log(this.plugin.settings.startday);
                 await this.plugin.saveSettings();
+                
                 const filePath = this.plugin.settings.workoutFolder ?? '/Workout';
                 //폴더 생성
                 try {
@@ -36,13 +42,14 @@ export class FirstWorkoutButtonModal extends Modal {
                     new Notice(error);
                 }
                 //폴더 내 메인 마크다운 파일 생성
-
+                // 이부분 stringify로 수정
+                
                 const workoutData = `
 ## Today Workout
 
 \`\`\`dataview
 TASK
-FROM "${filePath}/${this.plugin.settings.startday}"
+FROM "${filePath}"
 \`\`\`
 
 ## Workout Trend
@@ -56,6 +63,12 @@ FROM "${filePath}/${this.plugin.settings.startday}"
 `;
 
                 new Markdown(this.plugin, this.app).createNote('Workout Main', workoutData);
+
+                //루틴 플래너 코드 작성
+                new RoutineUpdate(this.plugin).routinePlanner();
+
+                //close 위치 변경
+                this.close();
             }),
         );
     }
